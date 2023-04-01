56a54b5f2d5af33cc110106f681349212375daa3
lliioollcn <lliiooll@126.com>
2023-04-01 13:50:44 +0800

    完成

--- versionPlugin/src/main/java/cn/lliiooll/dhl/plugin/PGPlugin.kt
+++ versionPlugin/src/main/java/cn/lliiooll/dhl/plugin/PGPlugin.kt
@@ -19,7 +19,7 @@ class PGPlugin : Plugin<Project> {
         if (!dir.exists()) {
             dir.mkdirs()
         }
-        if (!"go".exec() && !goBinDir.exists() || !"${goBinDir.absolutePath}\\go.exe".exec(goBinDir)) {
+        if (!goBinDir.exists() || !"${goBinDir.absolutePath}\\go.exe".exec(goBinDir)) {
             println("go未安装，正在自动安装...")
             val file = File(dir, "go.zip")
             if (!file.exists()) {
@@ -34,62 +34,92 @@ class PGPlugin : Plugin<Project> {
                 "tar -xzvf go.zip go".exec(dir)
             }
         }
-        if (!"gomobile".exec()) {
-            System.out.println("Gomobile不存在，自动安装...")
-            val srcDir = File(dir, "mobile-master")
-            if (srcDir.exists()) {
-                srcDir.delete()
-            }
-            val srcFile = File(dir, "sources.zip")
-            if (!srcFile.exists()) {
-                doDownload(
-                    "https://github.com/golang/mobile/archive/refs/heads/master.zip", srcFile
-                )
-                "tar -xzvf sources.zip".exec(dir)
-            }
-            val buildScript = File(srcDir, "build.bat")
-            val adminScript = File(srcDir, "admin.bat")
-            val goFile = File(goBinDir, "go.exe")
 
-            FileUtil.writeString(
-                "@echo off\n" + "set GOMODCACHE=${goBinDir.absolutePath}\\pkg\\mod\n" + "set GOPATH=${goBinDir.absolutePath}\n" + "${goFile.absolutePath} env -w GO111MODULE=on \n" + "${goFile.absolutePath} env -w GOPROXY=https://goproxy.cn\n" + "${goFile.absolutePath} build golang.org/x/mobile/cmd/gomobile\n" + "exit",
-                buildScript,
-                StandardCharsets.UTF_8
-            )
-            FileUtil.writeString(
-                "@echo off\n" + "%1 mshta vbscript:CreateObject(\"Shell.Application\").ShellExecute(\"cmd.exe\",\"/c %~s0 ::\",\"\",\"runas\",1)(window.close)&&exit\n" + "cd /d \"%~dp0\"\n" + "start build.bat\n" + "exit",
-                adminScript,
-                StandardCharsets.UTF_8
-            )
-
-            val gomobileFile = File(srcDir, "gomobile.exe")
-            if (gomobileFile.exists()) gomobileFile.delete()
-            adminScript.absolutePath.exec1(srcDir)
-            val startTime = System.currentTimeMillis()
-            while (!gomobileFile.exists()) {
-                Thread.sleep(1000L)
-                if (System.currentTimeMillis() - startTime > 1000 * 60 * 10) {
-                    throw RuntimeException("GoMobile编译失败，请手动启动 ${adminScript.absolutePath} 进行编译")
-                }
-            }
-            gomobileFile.absolutePath.exec()
-            println("安装成功，开始编译库...")
-            val gmcDir = File(project.projectDir, "../gmc")
-            val buildGMCScript = File(gmcDir, "buildGMC.bat")
-            FileUtil.writeString(
-                "@echo off\n" +
-                        "set GOMODCACHE=${goDir.absolutePath}\\pkg\\mod\n" +
-                        "set GOPATH=${goDir.absolutePath}\n" +
-                        "set PATH=${goBinDir.absolutePath}\n" +
-                        "set ANDROID_HOME=D:\\env\\androidSdk\n" +
-                        "${gomobileFile.absolutePath} init\n" +
-                        "${gomobileFile.absolutePath} bind -target=android ./service/gmc_android\n" +
-                        "exit",
-                buildGMCScript,
-                StandardCharsets.UTF_8
-            )
-            "".exec1(gmcDir)
+        System.out.println("Gomobile不存在，自动安装...")
+        val srcDir = File(dir, "mobile-master")
+        if (srcDir.exists()) {
+            srcDir.delete()
         }
+        val srcFile = File(dir, "sources.zip")
+        if (!srcFile.exists()) {
+            doDownload(
+                "https://github.com/golang/mobile/archive/refs/heads/master.zip", srcFile
+            )
+            "tar -xzvf sources.zip".exec(dir)
+        }
+        val buildScript = File(srcDir, "build.bat")
+        val adminScript = File(srcDir, "admin.bat")
+        val goFile = File(goBinDir, "go.exe")
+        val staticFile = File(dir, "static.zip")
+        if (!staticFile.exists()) {
+            doDownload(
+                "https://github.com/ProtobufBot/pbbot-react-ui/releases/latest/download/static.zip",
+                staticFile
+            )
+        }
+        "tar -xzvf static.zip".exec(File(goDir, "pkg\\static\\"))
+        FileUtil.writeString(
+            "@echo off\n" + "set GOMODCACHE=${goBinDir.absolutePath}\\pkg\\mod\n" + "set GOPATH=${goBinDir.absolutePath}\n" + "${goFile.absolutePath} env -w GO111MODULE=on \n" + "${goFile.absolutePath} env -w GOPROXY=https://goproxy.cn\n" + "${goFile.absolutePath} build golang.org/x/mobile/cmd/gomobile\n" + "exit",
+            buildScript,
+            StandardCharsets.UTF_8
+        )
+        FileUtil.writeString(
+            "@echo off\n" + "%1 mshta vbscript:CreateObject(\"Shell.Application\").ShellExecute(\"cmd.exe\",\"/c %~s0 ::\",\"\",\"runas\",1)(window.close)&&exit\n" + "cd /d \"%~dp0\"\n" + "start build.bat\n" + "exit",
+            adminScript,
+            StandardCharsets.UTF_8
+        )
+
+        val gomobileFile = File(srcDir, "gomobile.exe")
+        if (gomobileFile.exists()) gomobileFile.delete()
+        adminScript.absolutePath.exec1(srcDir)
+        var startTime = System.currentTimeMillis()
+        while (!gomobileFile.exists()) {
+            Thread.sleep(1000L)
+            if (System.currentTimeMillis() - startTime > 1000 * 60 * 10) {
+                throw RuntimeException("GoMobile编译失败，请手动启动 ${adminScript.absolutePath} 进行编译")
+            }
+        }
+        gomobileFile.absolutePath.exec()
+        println("安装成功，开始编译库...")
+        val gmcDir = File(project.projectDir, "../gmc")
+        val buildGMCScript = File(gmcDir, "buildGMC.bat")
+        val adminGMCScript = File(gmcDir, "adminGMC.bat")
+        FileUtil.writeString(
+            "@echo off\n" + "%1 mshta vbscript:CreateObject(\"Shell.Application\").ShellExecute(\"cmd.exe\",\"/c %~s0 ::\",\"\",\"runas\",1)(window.close)&&exit\n" + "cd /d \"%~dp0\"\n" + "start buildGMC.bat\n" + "exit",
+            adminGMCScript,
+            StandardCharsets.UTF_8
+        )
+        FileUtil.writeString(
+            "@echo off\n" +
+                    "set GOMODCACHE=${goDir.absolutePath}\\pkg\\mod\n" +
+                    "set GOPATH=${goDir.absolutePath}\n" +
+                    "set PATH=%PATH%;${goBinDir.absolutePath}\n" +
+                    "set ANDROID_HOME=D:\\env\\androidSdk\n" +
+                    "${gomobileFile.absolutePath} init\n" +
+                    "${gomobileFile.absolutePath} bind -target=android ./service/gmc_android\n" +
+                    "exit",
+            buildGMCScript,
+            StandardCharsets.UTF_8
+        )
+        val replaceText = File(gmcDir, "service\\gmc_android\\gmc.go")
+        FileUtil.writeLines(
+            FileUtil.readLines(replaceText, StandardCharsets.UTF_8).stream().filter {
+                return@filter !it.startsWith("//")
+            }.toList(), replaceText, StandardCharsets.UTF_8
+        )
+        adminGMCScript.absolutePath.exec1(gmcDir)
+        val aarFile = File(goFile, "gmc_android.aar")
+        if (aarFile.exists()) aarFile.delete()
+        startTime = System.currentTimeMillis()
+        while (!aarFile.exists()) {
+            Thread.sleep(1000L)
+            if (System.currentTimeMillis() - startTime > 1000 * 60 * 10) {
+                throw RuntimeException("GoMobile编译失败，请手动启动 ${adminGMCScript.absolutePath} 进行编译")
+            }
+        }
+        println("编译成功，正在初始化...")
+        FileUtil.copyFile(aarFile, File(project.projectDir, "libs\\" + aarFile.name))
+        println("初始化完毕")
 
 
     }
